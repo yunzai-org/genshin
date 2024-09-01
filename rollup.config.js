94339d8d26ccb4429d21434c6de7f1ecb2a71d8c
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-09-02 00:20:07 +0800

    feat: 降低pup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,44 @@
+import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
-import { defineConfig as createConfig } from 'yunzai/rollup'
-export default createConfig({
-  plugins: [
-    typescript({
-      compilerOptions: {
-        declaration: true,
-        declarationDir: 'lib/types'
-      },
-      include: ['src/**/*']
-    })
-  ]
-})
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import alias from '@rollup/plugin-alias'
+import dts from 'rollup-plugin-dts'
+import { Options } from 'yunzai/rollup'
+export default defineConfig([
+  {
+    ...Options,
+    plugins: [
+      // 处理ts文件
+      typescript({
+        compilerOptions: {
+          outDir: 'lib'
+        },
+        include: ['src/**/*']
+      })
+    ]
+  }
+  // {
+  //   ...Options,
+  //   plugins: [
+  //     // 处理别名
+  //     alias({
+  //       entries: [
+  //         {
+  //           find: '@',
+  //           replacement: resolve(dirname(fileURLToPath(import.meta.url)), 'src')
+  //         }
+  //       ]
+  //     }),
+  //     // 处理ts文件
+  //     typescript({
+  //       compilerOptions: {
+  //         outDir: 'lib'
+  //       },
+  //       include: ['src/**/*']
+  //     }),
+  //     // 所有的dts文件输出
+  //     dts()
+  //   ]
+  // }
+])
